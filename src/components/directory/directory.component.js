7bbc8414a8dbee2d39a4287ce201a1317c30b4e9
Majo <fake@fake.com>
2022-12-28 21:31:26 +0100

    First commit

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,16 @@
+import CategoryItem from "../category-item/category-item.components";
+
+import "./directory.styles.scss";
+
+const Directory = ({categories}) => {
+    return (
+        <div className='categories-container'>
+            {categories.map(({id, title, imageUrl}) => (
+                <CategoryItem key={id} title={title} imageUrl={imageUrl} />
+            ))}
+            }
+        </div>
+    )
+}
+
+export default Directory;
